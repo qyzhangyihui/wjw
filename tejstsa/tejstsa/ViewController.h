8b6801e5b51afdb23d8d229119021e14f2e39af2
qyzhangyihui <yihui.zhang@qyer.com>
2014-03-07 05:41:00 -0800

    1.commit

---
+++ tejstsa/tejstsa/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  tejstsa
+//
+//  Created by wangjianwei on 14-3-7.
+//  Copyright (c) 2014年 wangjianwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ tejstsa/tejstsa/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  tejstsa
+//
+//  Created by wangjianwei on 14-3-7.
+//  Copyright (c) 2014年 wangjianwei. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
